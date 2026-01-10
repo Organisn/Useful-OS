63b4deee4eb34ecef43c4c1e380d6225c521bf04
Snidi <emanuele.snidero@gmail.com>
2026-01-10 03:45:08 +0100

    Dialout group account application

--- Fedora/arduino.sh
+++ Fedora/arduino.sh
@@ -4,8 +4,12 @@
  # Permanent write rights:
  # Paste 
  SUBSYSTEMS=="usb", ATTRS{idVendor}=="2a03", GROUP="plugdev", MODE="0666"
-# (where "idVendor" can be found into pc connected device list)
+# (where the "idVendor" attribute can be found into pc connected device list)
 lsusb -vvv
 # into /etc/udev/rules.d/99-arduino.rules
 # Refresh 'udev' rules
 sudo udevadm control --reload
+# If not enough, add your actual account to dialout group
+sudo usermod -a -G dialout <username>
+# Username can be found by 
+whoami
